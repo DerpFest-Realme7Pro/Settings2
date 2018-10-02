b5dc0b0a75476c954d6c86e34b1d433aecbe79e5
IacobIonut01 <paulionut2003@gmail.com>
2018-10-02 20:14:22 +0300

    Settings UI: Import Redesigned UI [SQUASHED]
    Dashboard : Quick Redesign
    
    Change-Id: Ia8905dd1908ffba70ed88934d84f7abaf6c40d99
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
    
    Settings UI : Tint Storage ProgressBar & Redesign Toolbar
    
    Change-Id: I17fc049a0e8276cfcbc34e331581373b74239c51
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
    
    Settings UI : Show User Icon in dashboard
    
    Change-Id: Id02b6375087c3187a4d13388d5571b72184a01d1
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
    
    UI : Increase toolbar size and misc fixes
    
    Change-Id: Ie7768d99c7220f1098ecbcdf56f21747e3c8d146
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
    
    Some changes for our Setting Title
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
    
    Settings UI: Open UserSettings fragment on user icon click
    
    Change-Id: I24af7981ae6b360c445a5dec181c8cdf2bdedfb2
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
    
    Settings UI: Wait What !!
    
    * Adapt Settings UI to make compatible for AOSiP
    
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
    
    Co-authored-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Co-authored-by: DennySPB <dennyspb@gmail.com>
    Co-authored-by: marshmello61 <ultramayur123@gmail.com>

---
+++ src/com/android/settings/dashboard/DashboardProfileIcon.java
@@ -0,0 +1,57 @@
+package com.android.settings.dashboard;
+
+import android.content.Context;
+import android.os.UserManager;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.content.pm.UserInfo;
+
+import com.android.settings.R;
+import com.android.settingslib.Utils;
+import com.android.settings.core.SubSettingLauncher;
+import com.android.settings.users.UserSettings;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class DashboardProfileIcon extends RelativeLayout implements OnClickListener {
+
+    private View mUsers;
+
+    public DashboardProfileIcon(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+        public DashboardProfileIcon(Context context) {
+        super(context);
+        init(context);
+    }
+
+    private void init(Context context) {
+        View view = inflate(context, R.layout.item_dashboard_profile, this);
+        TextView name = view.findViewById(R.id.user_name);
+        ImageView icon = view.findViewById(R.id.user_icon);
+        UserManager userManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
+        UserInfo info = com.android.settings.Utils.getExistingUser(userManager, android.os.Process.myUserHandle());
+        LayoutParams lp = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        setLayoutParams(lp);
+        name.setText(info.name);
+        icon.setImageDrawable(Utils.getUserIcon(context, userManager, info));
+        mUsers = findViewById(R.id.dashboard_profile_icon);
+        mUsers.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mUsers) {
+            new SubSettingLauncher(getContext())
+                    .setDestination(UserSettings.class.getName())
+                    .setSourceMetricsCategory(MetricsProto.MetricsEvent.OWLSNEST)
+                    .launch();
+        }
+    }
+}
